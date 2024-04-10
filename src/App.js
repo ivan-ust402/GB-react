74e53678ea98325436c1f6c9675ad22b3e49073a
Ivan Ustyantsev <vangold915@gmail.com>
2024-04-10 22:38:37 +0700

    Today's work is completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
-function App() {
+function App({ myName }) {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <header className="App-header">
+        My First React App
+        <h3>Hello world!</h3>
+        <h3>My name is { myName }</h3>
+      </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
